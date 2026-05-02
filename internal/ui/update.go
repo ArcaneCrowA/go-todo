f20561b3d462f3529cc7f2d28300780659fcab05
Adilet <adiletbeishekeyev@gmail.com>
2026-05-02 13:28:41 +0500

    refactor: move logic for states outside update

--- internal/ui/update.go
+++ internal/ui/update.go
@@ -11,38 +11,45 @@ func (m TodoList) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	switch m.state {
 	case listView:
-		switch msg := msg.(type) {
-		case []task.Item:
-			m.list = msg
-			return m, nil
-		case tea.KeyPressMsg:
-			switch msg.String() {
-			case "ctrl+c", "q":
-				return m, tea.Quit
-			case "up", "k":
-				if m.cursor > 0 {
-					m.cursor--
-				}
-			case "down", "j":
-				if m.cursor < len(m.list)-1 {
-					m.cursor++
-				}
-			case "e":
-				m.state = editView
-				m.textInput.Focus()
-				return m, textinput.Blink
-			}
-			m.list, _ = m.storage.Load()
-		}
+		m.listUpdate(msg, &cmd)
 	case editView:
-		switch msg := msg.(type) {
-		case tea.KeyPressMsg:
-			if msg.String() == "enter" {
-				m.textInput.Blur()
-				m.state = listView
-			}
-		}
-		m.textInput, cmd = m.textInput.Update(msg)
+		m.editUpdate(msg, &cmd)
 	}
 	return m, cmd
+}
+
+func (m *TodoList) listUpdate(msg tea.Msg, cmd *tea.Cmd) {
+	switch msg := msg.(type) {
+	case []task.Item:
+		m.list = msg
+	case tea.KeyPressMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			*cmd = tea.Quit
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+		case "down", "j":
+			if m.cursor < len(m.list)-1 {
+				m.cursor++
+			}
+		case "e":
+			m.state = editView
+			m.textInput.Focus()
+			*cmd = textinput.Blink
+		}
+		m.list, _ = m.storage.Load()
+	}
+}
+
+func (m *TodoList) editUpdate(msg tea.Msg, cmd *tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyPressMsg:
+		if msg.String() == "enter" {
+			m.textInput.Blur()
+			m.state = listView
+		}
+	}
+	m.textInput, *cmd = m.textInput.Update(msg)
 }
